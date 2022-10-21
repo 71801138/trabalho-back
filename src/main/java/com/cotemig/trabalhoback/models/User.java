08d6eab3f62c424a93cdf8854c7c11e2635cbf71
ALAN_CESAR_DE_SOUZA <71801138@SIGMAB19.BARROCA>
2022-10-20 22:13:10 -0300

    Creates user model

---
+++ src/main/java/com/cotemig/trabalhoback/models/User.java
@@ -0,0 +1,26 @@
+package com.cotemig.trabalhoback.models;
+
+public class User {
+    private String name = "";
+    private String imageUrl = "";
+
+    public User(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
