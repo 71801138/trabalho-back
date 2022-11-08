938f2854f6472f72b12b297835e2d2ef894434b4
LUCAS_OLIVEIRA_LOPES <72100559@SIGMAB20.BARROCA>
2022-11-07 21:38:15 -0300

    Working: Create logic task and state

--- src/main/java/com/cotemig/trabalhoback/services/task/TaskService.java
+++ src/main/java/com/cotemig/trabalhoback/services/task/TaskService.java
@@ -1,20 +1,36 @@
 package com.cotemig.trabalhoback.services.task;
 
+import com.cotemig.trabalhoback.models.User;
 import com.cotemig.trabalhoback.models.task.Task;
 import com.cotemig.trabalhoback.repository.task.taskRepository;
+import com.cotemig.trabalhoback.services.task.state.AbstractState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class TaskService {
+
   @Autowired
   private taskRepository taskRepository;
 
-  protected AbstractState getCurrentState(){
-
+  public  Task create(Task task, Long userId, Long typeId){
+    return null;
   }
 
-  public Task createTask(Task task){
+  private AbstractState currentState(Task task){
+    return task.status = new task.status();
+  }
+
+  public Boolean nextStep(Long taskId)
+  {
+    Optional<Task> currentTask = this.taskRepository.findById(taskId);
+    AbstractState currentState = this.currentState(currentTask);
+    this.taskRepository.changeStatus(currentState);
+  }
+
+  private Task createTask(Task task){
     Task taskCreated = taskRepository.save(task);
     return taskCreated;
   }
